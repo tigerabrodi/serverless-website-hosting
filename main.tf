9608bc61b3948788286cacbb362d20785397e6cc
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-16 14:39:34 +0100

    s3 bucket with public read

--- main.tf
+++ main.tf
@@ -20,3 +20,47 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "tiger-kun-website-bucket"
+
+}
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket_public_access_block" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "website_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website_bucket_ownership_controls,
+    aws_s3_bucket_public_access_block.website_bucket_public_access_block
+  ]
+
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "website_bucket_website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
