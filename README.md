b0b6644e34add6a951678d6ddaf5e096b51be8dc
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-16 14:18:41 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Serverless Website Hosting
---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  cloud {
+    organization = "tiger_projects"
+    workspaces {
+      name = "serverless-website-hosting"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
